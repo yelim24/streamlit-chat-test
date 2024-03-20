6823b80e5a2bb4c7da642ebf94fd4e338ceb8739
yelimkim <yelimkim@hufs.ac.kr>
2024-03-20 12:14:08 +0900

    add val

--- main.py
+++ main.py
@@ -133,9 +133,12 @@ if prompt := st.chat_input("당신의 고민을 말씀해주세요"):
             dialog_step = bot_response_list[0].split(':')[-1].strip()
             bot_response = bot_response_list[1]
         
+        text = ''
+        
         for char in bot_response:
             time.sleep(0.05)
-            message_placeholder.markdown(char + "▌")
+            text += char
+            message_placeholder.markdown(text + "▌")
         
             
             # if len(msg_split_list) == 2:
